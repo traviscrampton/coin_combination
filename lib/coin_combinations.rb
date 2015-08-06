f1856b3178696303407b69e46ec4ced8837fce4f
Travis Crampton & Jennifer Bell <student@epicodus.com>
2015-08-06 13:49:31 -0700

    first until loop quarters

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -2,10 +2,27 @@ class Fixnum
   define_method(:coin_combo) do
 
     # coin_values = { "quarter" => 25, "dime" => 10, "nickle" => 5, "pennies" => 1 }
-    coin_amount = self/25
-    coin_remain = self%25
-    if coin_remain == 0
-      coin_amount.to_s().concat(" quarters")
+    # coin_amount = self/25
+    # coin_remain = self%25
+    # if coin_remain == 0
+    #   coin_amount.to_s().concat(" quarters")
+    #
+    #
+    # elsif coin_remain > 0 && coin_remain%10 == 0
+    #
+    #   coin_amount.to_s().concat(" dimes")
+
+
+    quarters = 0
+    dimes = 0
+    nickles = 0
+    pennies = 0
+
+    change = self
+    until change.<(25)
+      change = change.-(25)
+      quarters = quarters.+(1)
     end
+    quarters
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -9,7 +9,10 @@ require('coin_combinations')
 #  it("tells user how many quarters to use") do
 #    expect(100.coin_combo()).to(eq(0))
 #  end
-  it('finds amount of quarters and labels') do
-  expect(100.coin_combo()).to(eq("4 quarters"))
-  end
+  # it('finds amount of quarters and labels') do
+  # expect(100.coin_combo()).to(eq("4 quarters"))
+  # end
+  it('counts the amount of quarters') do
+    expect(50.coin_combo()).to(eq(2))
+end
 end
