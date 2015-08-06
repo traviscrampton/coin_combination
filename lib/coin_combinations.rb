e37db683640d890cbe95ede7edba5af8ac1126b6
Travis Crampton & Jennifer Bell <student@epicodus.com>
2015-08-06 11:51:23 -0700

    Third spec adding the string quarters

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -2,9 +2,10 @@ class Fixnum
   define_method(:coin_combo) do
 
     # coin_values = { "quarter" => 25, "dime" => 10, "nickle" => 5, "pennies" => 1 }
-    coin_amount = self
-
-    coin_amount % 25
-
+    coin_amount = self/25
+    coin_remain = self%25
+    if coin_remain == 0
+      coin_amount.to_s().concat(" quarters")
+    end
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -6,7 +6,10 @@ require('coin_combinations')
 #     expect(50.coin_combo()).to(eq(2))
 #   end
 
-  it("tells user how many quarters to use") do
-    expect(100.coin_combo()).to(eq(0))
+#  it("tells user how many quarters to use") do
+#    expect(100.coin_combo()).to(eq(0))
+#  end
+  it('finds amount of quarters and labels') do
+  expect(100.coin_combo()).to(eq("4 quarters"))
   end
 end
